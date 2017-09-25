70240b8302e2127b5a3d166b89f584a31d1dd9d8
Ganuelas, Edward (TOR-MCL) <Edward.Ganuelas@maclaren.com>
2017-09-25 13:25:00 -0400

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/main.js',
+    entry: ['babel-polyfill','./js/main.js'],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
     }
 };

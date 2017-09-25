ec889db458d6fc58578810a61f2a7a890815923a
Ganuelas, Edward (TOR-MCL) <Edward.Ganuelas@maclaren.com>
2017-09-25 15:30:06 -0400

    add vue-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            {test: /\.vue$/, loader: 'vue-loader'}
         ]
     },
     resolve:{

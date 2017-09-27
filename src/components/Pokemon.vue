f29574b5fbb9b353ae2e17784000a14f27c0b3b2
Ganuelas, Edward (TOR-MCL) <Edward.Ganuelas@maclaren.com>
2017-09-27 16:44:50 -0400

    Filtering flavour text, initial attempt

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,12 +1,34 @@
 <template>
     <h2>
-        {{pokemonData.name | capitalize}}</h2>
+        {{pokemonData.name | capitalize}} {{getFlavourText('test')}}</h2>
+
 </template>
 
 <script>
 export default {
     name: 'pokemon',
     props: ['pokemonData'],
+    data: function() {
+        return {
+            "flavorText": this.pokemonData.flavor_text_entries
+        }
+    },
+    methods: {
+        getFlavourText : function (region) {
+           
+           let flvText = '';
+
+           for(let obj in this.flavorText){
+            //    console.log(this.flavorText[obj].language.name);
+                if(this.flavorText[obj].language.name === 'en'){
+                    flvText = this.flavorText[obj].flavor_text;
+                    break;
+                }
+           }
+           return flvText;
+
+        }
+    }
 }
 </script>
 

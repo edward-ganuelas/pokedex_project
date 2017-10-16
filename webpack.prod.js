f1610060a9e6f392c7e7e3de79fab66c343e333f
Ganuelas, Edward (TOR-MCL) <Edward.Ganuelas@maclaren.com>
2017-10-16 13:48:43 -0400

    Update configs

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,13 +5,18 @@ const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
     devtool: 'source-map',
+    resolve:{
+        alias:{
+            vue: 'vue/dist/vue.min.js'
+        }
+    },
     plugins: [
         new UglifyJSPlugin({
             sourceMap: true
         }),
         new webpack.DefinePlugin({
             'process.env': {
-                'NODE_ENV': JSON.stringify('production')
+                NODE_ENV: '"production"'
             }
         })
     ]

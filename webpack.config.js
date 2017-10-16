5ff49399e9357b9f06025d22eea2f6cc76713abd
Ganuelas, Edward (TOR-MCL) <Edward.Ganuelas@maclaren.com>
2017-10-16 13:40:35 -0400

    Added webpack prod config

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,6 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    devServer: {
-      contentBase: './dist'  
-    },
     module: {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },

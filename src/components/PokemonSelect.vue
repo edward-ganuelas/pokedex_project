ac94e8681c80ad636391e4c0e44b5dee8456e273
Edward Ganuelas <epganuelas@gmail.com>
2017-11-04 11:58:13 -0400

    Styling updates, fix flavour text bug

--- src/components/PokemonSelect.vue
+++ src/components/PokemonSelect.vue
@@ -4,9 +4,6 @@
             <v-flex xs12>
                 <label for="pokemonSelect">Select Pokemon</label>
                 <v-select v-model="url" id="pokemonSelect" :options="pokemon" v-if="pokemon.length > 0">
-                    <!-- <option v-for="pokemon in pokedexResult" v-bind:value="pokemon.pokemon_species.url" v-bind:key="pokemon.entry_number">
-                        {{pokemon.pokemon_species.name | capitalize}}
-                    </option> -->
                 </v-select>
             </v-flex>
         </v-layout>

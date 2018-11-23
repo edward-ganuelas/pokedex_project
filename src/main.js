9273ae7988563e4ad98d3d28fa9679c46d83bad5
Ganuelas, Edward (TOR-MCL) <Edward.Ganuelas@maclaren.com>
2018-11-23 16:41:24 -0500

    add vue-cli-3 boilerplate

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
-import Vue from 'vue';
-import App from './components/App.vue';
-import 'bootstrap/dist/js/bootstrap';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 import Vuetify from 'vuetify';
 require('../node_modules/vuetify/dist/vuetify.min.css');
 
+Vue.config.productionTip = false
 Vue.use(Vuetify);
-
-Vue.filter('capitalize', function (value) {
-    if (!value) return ''
-    value = value.toString()
-    return value.charAt(0).toUpperCase() + value.slice(1)
-});
-
-var pokeDexVue = new Vue({
-    el: "#pokedex",
-    render: h => h(App)
-});
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')

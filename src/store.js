65e07dc22273491307ad14aee9f9912e2c948bb9
Edward Ganuelas <epganuelas@gmail.com>
2018-11-28 20:16:20 -0500

    added vuex persisted state

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -22,5 +23,6 @@ export default new Vuex.Store({
   },
   actions: {
 
-  }
+  },
+  plugins: [createPersistedState()]
 })

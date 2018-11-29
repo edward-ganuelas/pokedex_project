38f48d528c019651503339f9439a3e2696ab1051
Ganuelas, Edward (TOR-MCL) <Edward.Ganuelas@maclaren.com>
2018-11-29 14:26:28 -0500

    styling back button

--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -23,7 +23,11 @@
         </div>
         <div class="row">
             <div class="col-6">
-                <router-link to="/">Back</router-link>
+                <router-link to="/" class="back"><span class="sr-only">Back</span>
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="#000000" d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z" />
+                    </svg>
+                </router-link>
             </div>
         </div>
     </div>
@@ -124,5 +128,9 @@ export default{
 .name{
     text-transform: uppercase;
 }
-
+.back{
+    margin: 18px auto;
+    display: block;
+    text-align: center;
+}
 </style>

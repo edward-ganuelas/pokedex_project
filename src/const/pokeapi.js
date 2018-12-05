097916ae5746d07dec1a79e8edb9fc29445593b1
Edward Ganuelas <epganuelas@gmail.com>
2018-12-04 20:41:19 -0500

    revert api

--- src/const/pokeapi.js
+++ src/const/pokeapi.js
@@ -1,4 +1,4 @@
-export const POKEDEX = "http://localhost:8282/https://pokeapi.co/api/v2/pokedex/";
-export const POKEMONVERSION = "http://localhost:8282/https://pokeapi.co/api/v2/version/";
-export const POKEMON = "http://localhost:8282/https://pokeapi.co/api/v2/pokemon/";
-export const NATIONALDEX = "http://localhost:8282/https://pokeapi.co/api/v2/pokedex/1"
+export const POKEDEX = "https://pokeapi.co/api/v2/pokedex/";
+export const POKEMONVERSION = "https://pokeapi.co/api/v2/version/";
+export const POKEMON = "https://pokeapi.co/api/v2/pokemon/";
+export const NATIONALDEX = "https://pokeapi.co/api/v2/pokedex/1"
